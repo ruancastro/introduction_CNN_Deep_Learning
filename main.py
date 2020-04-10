3c677fa9309c410f395ac91d8f5dd77ed7204613
ruancastro <ruancastro15@gmail.com>
2020-04-10 11:54:32 -0300

    alterac plot

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-# TensorFlow e tf.keras
+#  Importando as bibliotecas 
 import tensorflow as tf
-# print("Teste")
 from tensorflow import keras
-
-# Librariesauxiliares
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,49 +10,53 @@ import matplotlib.pyplot as plt
 
 fashion_mnist = keras.datasets.fashion_mnist
 
+# não entendi a sintaxe dessa linha a baixo
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-# len(train_labels)
+# print(len(train_labels))
 # print(train_images.shape)
 # print(test_images.shape)
-# print(len(train_labels))
-# print("iai")
 
 fig=plt.figure()
 plt.imshow(train_images[0])
 plt.colorbar()
 plt.grid(False)
-# plt.show()
-plt.savefig('output/teste.png')
+plt.savefig('output/img_predict.png')
+plt.show()
+
 train_images = train_images / 255.0
 test_images = test_images / 255.0
-# plt.savefig('output/teste2.png')
 
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     # plt.imshow(train_images[i], cmap=plt.cm.binary)
-#     plt.xlabel(class_names[train_labels[i]])
-#     plt.savefig('output/teste2.png')
-# # plt.show()
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    # plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.imshow(train_images[i])
+    plt.xlabel(class_names[train_labels[i]])
+    plt.savefig('output/25_primeiras_treino.png')
+plt.show()
+
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
+    # keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(228, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=100)
+model.fit(train_images, train_labels, epochs=25)
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 print('\nTest accuracy:', test_acc)
+predictions = model.predict(test_images)
+print(np.argmax(predictions[0]))
